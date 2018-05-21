dbebfec5b7e653a87bf2bed9e32be2557c673b73
alnidok <alnidok@gmail.com>
2018-05-21 19:32:32 +0300

    social-аккаунты в конфиги

--- holidays/article.php
+++ holidays/article.php
@@ -1,21 +1,15 @@
 <?php
 
-//use app\assets\ShareAsset;
 use app\components\widgets\Breadcrumbs;
-use app\models\Holidays;
 use yii\helpers\Url;
-
-//ShareAsset::register($this);
 
 $this->title = "{$article->name}. {$article->country->name} ".date('Y');
 $description = $article->getDescription();
 $image = $article->getImage();
 
-//$this->registerJsFile('@web/js/share.js');
-//$this->registerCssFile('@web/css/sharer.css');
 $this->registerLinkTag(['rel' => 'canonical', 'href' => Url::canonical()]);
 $this->registerMetaTag(['name' => 'description', 'content' => $description]);
-$this->registerMetaTag(['name' => 'keywords', 'content' => ""]);
+$this->registerMetaTag(['name' => 'keywords', 'content' => '']);
 $this->registerMetaTag(['property' => 'og:locale', 'content' => 'ru_RU']);
 $this->registerMetaTag(['property' => 'og:url', 'content' => Url::canonical()]);
 $this->registerMetaTag(['property' => 'og:title', 'content' => $this->title]);
@@ -25,14 +19,14 @@ $this->registerMetaTag(['property' => 'og:type', 'content' => 'article']);
 $this->registerMetaTag(['property' => 'og:site_name', 'content' => Yii::$app->name]);
 $this->registerMetaTag(['property' => 'og:updated_time', 'content' => date('c', $article->updated_at)]);
 
-$this->registerMetaTag(['name' => 'twitter:twitter:card', 'content' => 'summary_large_image']);
-$this->registerMetaTag(['name' => 'twitter:twitter:title', 'content' => $this->title]);
-$this->registerMetaTag(['name' => 'twitter:twitter:description', 'content' => $description]);
-$this->registerMetaTag(['name' => 'twitter:twitter:image', 'content' => $image]);
-$this->registerMetaTag(['name' => 'twitter:twitter:site', 'content' => '@account-travel']);
-$this->registerMetaTag(['name' => 'twitter:twitter:creator', 'content' => '@account-travel']);
+$this->registerMetaTag(['name' => 'twitter:card', 'content' => 'summary_large_image']);
+$this->registerMetaTag(['name' => 'twitter:title', 'content' => $this->title]);
+$this->registerMetaTag(['name' => 'twitter:description', 'content' => $description]);
+$this->registerMetaTag(['name' => 'twitter:image', 'content' => $image]);
+$this->registerMetaTag(['name' => 'twitter:site', 'content' => Yii::$app->params['social']['twitter']]);
+//$this->registerMetaTag(['name' => 'twitter:creator', 'content' => Yii::$app->params['social']['twitter']]);
 
-$this->registerMetaTag(['property' => 'article:publisher', 'content' => 'https://facebook.com/account-travel']);
+$this->registerMetaTag(['property' => 'article:publisher', 'content' => 'https://facebook.com/'.Yii::$app->params['social']['facebook']]);
 $this->registerMetaTag(['property' => 'article:published_time', 'content' => date('c', $article->created_at)]);
 $this->registerMetaTag(['property' => 'article:modified_time', 'content' => date('c', $article->updated_at)]);
 $this->registerMetaTag(['property' => 'article:tag', 'content' => 'праздники']);
@@ -55,16 +49,6 @@ $this->registerMetaTag(['property' => 'article:tag', 'content' => 'праздн�
             <div class="col-md-11 col-lg-9">
                 <span class="lead"><?=$article->date()?></span>
                 <h1 class="display-4"><?= $article->name ?></h1>
-
-                <?php /*var_dump($dates) */?>
-
-                <!--<div class="media blog-post-author">
-                    <img alt="Image" src="<?/*=$article->author->getPhoto()*/?>" class="avatar avatar-sm mr-3">
-                    <div class="media-body">
-                        <span class="h6"><?/*=$article->author->getFullName()*/?></span>
-                        <span class="text-small"><?/*=$article->getCreatedDate()*/?> • Время чтения <?/*=$article->getReadingTime()*/?></span>
-                    </div>
-                </div>-->
             </div>
         </div>
     </div>
